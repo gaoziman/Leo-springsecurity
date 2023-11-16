d4d47b7a8429a3d9928e7e80327e6fdd50d8459d
“gaoziman” <2942894660@qq.com>
2023-11-16 14:19:29 +0800

    自定义登录用户名跟密码

---
+++ Leo-springsecurity6-start/src/main/java/org/javatop/Application.java
@@ -0,0 +1,36 @@
+package org.javatop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+/**
+ * @author : Leo
+ * @version 1.0
+ * @date 2023-10-31 15:05
+ * @description : 启动类
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class Application
+{
+
+    public static void main(String[] args)
+    {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        Environment environment = context.getBean(Environment.class);
+
+        System.out.println("访问链接：http://localhost:" + environment.getProperty("server.port"));
+        System.out.println("(♥◠‿◠)ﾉﾞ  项目启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+}
---
+++ Leo-springsecurity6-start/src/main/java/org/javatop/ss6/controller/HelloController.java
@@ -0,0 +1,23 @@
+package org.javatop.ss6.controller;
+
+import jakarta.servlet.Filter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Leo
+ * @version 1.0
+ * @date 2023-10-31 15:02
+ * @description :
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+
+         return "hello......";
+    }
+
+}
