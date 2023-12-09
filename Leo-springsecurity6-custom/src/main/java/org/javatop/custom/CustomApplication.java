1f00815f1579bdcdbb30a8d7d44a2d36c207f5a5
gaoziman <2942894660@qq.com>
2023-12-09 17:28:37 +0800

    创建自定义模块

---
+++ Leo-springsecurity6-custom/src/main/java/org/javatop/custom/CustomApplication.java
@@ -0,0 +1,24 @@
+package org.javatop.custom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+/**
+ * @author : Leo
+ * @version 1.0
+ * @date 2023-12-09 17:23
+ * @description :
+ */
+@SpringBootApplication
+public class CustomApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(CustomApplication.class, args);
+        Environment environment = context.getBean(Environment.class);
+
+        System.out.println("访问链接：http://localhost:" + environment.getProperty("server.port"));
+        System.out.println("(♥◠‿◠)ﾉﾞ  项目启动成功 ლ(´ڡ`ლ)ﾞ \n");
+
+    }
+}
---
+++ Leo-springsecurity6-custom/src/main/java/org/javatop/custom/controller/HelloController.java
@@ -0,0 +1,20 @@
+package org.javatop.custom.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Leo
+ * @version 1.0
+ * @date 2023-12-09 17:26
+ * @description :
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
