0218f1ea8f82f6f677c4ad46f91c9a2f58807cb5
gaoziman <2942894660@qq.com>
2023-12-10 23:24:33 +0800

    登录成功之后的处理

---
+++ Leo-springsecurity6-custom/src/main/java/org/javatop/custom/config/MyAuthenticationSuccessHandle.java
@@ -0,0 +1,31 @@
+package org.javatop.custom.config;
+
+import cn.hutool.core.lang.hash.Hash;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * @author : Leo
+ * @version 1.0
+ * @date 2023-12-10 22:03
+ * @description : 响应成功之后的JSON
+ */
+public class MyAuthenticationSuccessHandle implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("msg", "登录成功");
+        result.put("code", 200);
+
+        response.setContentType("application/json;charset=utf-8");
+        new ObjectMapper().writeValue(response.getWriter(), result);
+    }
+}
--- Leo-springsecurity6-custom/src/main/java/org/javatop/custom/config/MySecurityConfig.java
+++ Leo-springsecurity6-custom/src/main/java/org/javatop/custom/config/MySecurityConfig.java
@@ -19,15 +19,16 @@ public class MySecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .requestMatchers("/test").permitAll() // 放行改资源，不用认证可以直接访问
+//                .requestMatchers("/test").permitAll() // 放行改资源，不用认证可以直接访问
                 .requestMatchers("/login.html").permitAll()
                 .anyRequest().authenticated() // 所有请求都需要认证
                 .and().formLogin() // 开启表单登录
                 .loginPage("/login.html") //我们自定义的登录页面
                 .loginProcessingUrl("/doLogin") // 处理登录的url
                 .usernameParameter("username")
-                .passwordParameter("password")
-
+                .passwordParameter("password") //
+//                .successForwardUrl("/hello") 访问默认的接口
+                .defaultSuccessUrl("/test") // 依然会访问到原来访问的接口
         ;
         return http.build();
     }
