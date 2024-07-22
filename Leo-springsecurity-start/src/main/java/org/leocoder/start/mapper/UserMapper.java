d95f759ac156303111f0e0690852cd4bf51fee8e
gaoziman <2942894660@qq.com>
2024-07-22 23:01:53 +0800

    fix/ 删除多余模块

--- Leo-springsecurity-start/src/main/java/org/leocoder/start/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package org.leocoder.start.mapper;
-
-
-import org.leocoder.start.domain.User;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface UserMapper extends BaseMapper<User> {
-}
