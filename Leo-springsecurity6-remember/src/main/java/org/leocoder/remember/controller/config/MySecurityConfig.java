1cd76d72b5c4ccebf9426603b7c08cf4324912c8
gaoziman <2942894660@qq.com>
2024-08-14 09:12:17 +0800

    feat/ 持久化令牌方式

--- Leo-springsecurity6-remember/src/main/java/org/leocoder/remember/controller/config/MySecurityConfig.java
+++ Leo-springsecurity6-remember/src/main/java/org/leocoder/remember/controller/config/MySecurityConfig.java
@@ -1,10 +1,18 @@
 package org.leocoder.remember.controller.config;
 
+import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 /**
  * @author : Leo
@@ -16,6 +24,23 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class MySecurityConfig {
 
+    @Resource
+    private DataSource dataSource;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(User.withUsername("admin").password("{noop}123456").roles("admin").build());
+    }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        // 启动时创建表结构，仅在第一次启动时使用
+        // jdbcTokenRepository.setCreateTableOnStartup(true);
+        return jdbcTokenRepository;
+    }
+
     // 自定义表单认证
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -26,6 +51,8 @@ public class MySecurityConfig {
                 // 开启remember me功能
                 .and()
                 .rememberMe()
+                // 持久化令牌
+                .tokenRepository(persistentTokenRepository())
                 .and()
 
                 .logout((logout) -> logout
